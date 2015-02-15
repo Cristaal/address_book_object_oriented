4ff5142fded625b7ef7c4b34704dd00420c7bbb1
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-14 19:21:00 -0800

    add updated html, css, and scripts files

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,11 @@ $(document).ready(function() {
     $("input#new-address").val("");
   });
 });
+
+$(".contact").last().click(function() {
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+  $(".address").text(newContact.address);
+});
